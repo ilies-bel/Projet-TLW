dab94b4391b556e82ac2364016df207cddd204ac
otakouilles <56508691+otakouilles@users.noreply.github.com>
2020-02-23 17:09:19 +0100

    Add files via upload

--- Mini projet web/Code/JS/main.js
+++ Mini projet web/Code/JS/main.js
@@ -2,8 +2,25 @@
 function myFunction() {
 	var x = document.getElementById("myTopnav");
 	if (x.className === "topnav") {
-	  x.className += " responsive";
+		x.className += " responsive";
 	} else {
-	  x.className = "topnav";
+		x.className = "topnav";
 	}
-  }
+}
+
+function sendMail() {
+
+
+	var input = document.getElementById('mail_texte');
+	texte = input.value;
+	var input = document.getElementById('mail_sujet');
+	sujet = input.value;
+	var input = document.getElementById('mail_nom');
+	nom = input.value;
+
+
+	window.location.href = "mailto:beldjilali.ilies@gmail.com?subject=" + sujet + "&body=" + texte + "%0A%0AMail envoyé par " + nom + " sur destineo voyage.";
+
+
+}
+

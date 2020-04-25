dfcfcb32672cf155eab7d2c1a4f6ebcd0623e6b5
Enagnon94 <61350648+Enagnon94@users.noreply.github.com>
2020-04-25 19:24:51 +0200

    25/04 19.24

--- Mini projet web/Code/JS/main.js
+++ Mini projet web/Code/JS/main.js
@@ -60,7 +60,8 @@ function sendMail() {
 
 
 
-//fonctiond e mise en place de l'ancre de retour en haut de page
+
+//fonction de mise en place de l'ancre de retour en haut de page
 jQuery(function(){
 	$(function () {
 		$(window).scroll(function () {
@@ -247,4 +248,60 @@ function calculateTotal() {
 
 	document.getElementById('totalPrice').innerHTML = total;
 	totalEl.style.display = 'block';
+}
+
+
+
+//----------------------------------------------------------------------------------Validation formulaire de contact
+
+
+
+function validateForm()                                    
+{ 
+var name = document.forms["myForm"]["name"];               
+var email = document.forms["myForm"]["email"];    
+var message = document.forms["myForm"]["message"];   
+
+if (name.value == "")                                  
+{ 
+	document.getElementById('errorname').innerHTML="Veuillez entrez un nom valide";  
+	name.focus(); 
+	return false; 
+}else{
+	document.getElementById('errorname').innerHTML="";  
+}
+   
+if (email.value == "")                                   
+{ 
+	document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+	email.focus(); 
+	return false; 
+}else{
+	document.getElementById('erroremail').innerHTML="";  
+}
+
+if (email.value.indexOf("@", 0) < 0)                 
+{ 
+	document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+	email.focus(); 
+	return false; 
+} 
+
+if (email.value.indexOf(".", 0) < 0)                 
+{ 
+	document.getElementById('erroremail').innerHTML="Veuillez entrez une adresse mail valide"; 
+	email.focus(); 
+	return false; 
+} 
+
+if (message.value == "")                           
+{
+	document.getElementById('errormsg').innerHTML="Veuillez entrez un message valide"; 
+	message.focus(); 
+	return false; 
+}else{
+	document.getElementById('errormsg').innerHTML="";  
+}
+
+return true; 
 }
